7bec64ce766e75bcf1e9f6c038c83df594128828
irbansin <ani.expo10@gmail.com>
2022-06-29 01:04:37 +0530

    ground and sky made

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { BallComponent } from './ball/ball.component';
+import { BackgroundComponent } from './background/background.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, BallComponent],
+  declarations: [AppComponent, HelloComponent, BallComponent, BackgroundComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
