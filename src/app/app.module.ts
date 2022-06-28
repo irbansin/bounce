6fef4b13d7955dce52babd5f8ade4fa7c8169b02
irbansin <ani.expo10@gmail.com>
2022-06-29 02:29:34 +0530

    login component working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { BallComponent } from './ball/ball.component';
 import { BackgroundComponent } from './background/background.component';
+import { LoginModule } from './login/login.module';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, LoginModule],
   declarations: [AppComponent, HelloComponent, BallComponent, BackgroundComponent],
   bootstrap: [AppComponent],
 })
